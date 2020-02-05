65deb7c8b6d803f1beafc5cdff744387a5ae1e25
Vitor Hugo Nascimento Pinto <vhpinto@stefanini.com>
2020-02-05 15:22:48 -0300

    Updating header and footer

--- src/layout/AppFooter.vue
+++ src/layout/AppFooter.vue
@@ -7,9 +7,49 @@
     <div class="container">
       <nav>
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>
+            <a
+              rel="tooltip"
+              title="Follow us on Twitter"
+              data-placement="bottom"
+              href
+              target="_blank"
+            >
+              <i class="fab fa-twitter"></i>
+              <p class="d-lg-none d-xl-none">Twitter</p>
+            </a>
+          </li>
+          <li>
+            <a
+              rel="tooltip"
+              title="Like us on Facebook"
+              data-placement="bottom"
+              href
+              target="_blank"
+            >
+              <i class="fab fa-facebook-square"></i>
+              <p class="d-lg-none d-xl-none">Facebook</p>
+            </a>
+          </li>
+          <li>
+            <a
+              rel="tooltip"
+              title="Follow us on Instagram"
+              data-placement="bottom"
+              href
+              target="_blank"
+            >
+              <i class="fab fa-instagram"></i>
+              <p class="d-lg-none d-xl-none">Instagram</p>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://demos.creative-tim.com/vue-now-ui-kit/documentation"
+              target="_blank"
+              class="dropdown-item"
+            >Documentation</a>
+          </li>
         </ul>
       </nav>
       <div class="copyright">&copy; {{ year }}</div>
--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -9,83 +9,18 @@
     <template>
       <router-link v-popover:popover1 class="navbar-brand" to="/index">Frontend</router-link>
     </template>
-    <template slot="navbar-menu">
-      <!-- <li class="nav-item">
-        <a
-          class="nav-link"
-          href="https://www.creative-tim.com/product/vue-now-ui-kit"
-          target="_blank"
-        >
-          <i class="now-ui-icons arrows-1_cloud-download-93"></i>
-          <p>Download</p>
-        </a>
-      </li>-->
-      <drop-down tag="li" title="Components" icon="now-ui-icons design_app" class="nav-item">
-        <a
-          href="https://demos.creative-tim.com/vue-now-ui-kit/documentation"
-          target="_blank"
-          class="dropdown-item"
-        >
-          <i class="now-ui-icons design_bullet-list-67"></i> Documentation
-        </a>
-      </drop-down>
-      <drop-down tag="li" title="Páginas" class="nav-item">
-        <div v-if="loggedIn">
-          <nav-link v-for="(item, index) in loggedInLinks" :key="index" :to="item.path">
-            <i :class="item.icon"></i>
-            {{item.title}}
-          </nav-link>
-
-          <a class="dropdown-item" @click="logout()">
-            <i class="now-ui-icons ui-1_simple-remove"></i> Sair
-          </a>
-        </div>
-        <div v-else>
-          <nav-link v-for="(item, index) in loggedOutLinks" :key="index" :to="item.path">
-            <i :class="item.icon"></i>
-            {{item.title}}
-          </nav-link>
-        </div>
-      </drop-down>
-
-      <li class="nav-item">
-        <a
-          class="nav-link"
-          rel="tooltip"
-          title="Follow us on Twitter"
-          data-placement="bottom"
-          href
-          target="_blank"
-        >
-          <i class="fab fa-twitter"></i>
-          <p class="d-lg-none d-xl-none">Twitter</p>
-        </a>
+    <template slot="navbar-menu" v-if="loggedIn">
+      <li class="nav-item" v-for="(item, index) in loggedInLinks" :key="index">
+        <router-link :to="item.path" class="nav-link">{{item.title}}</router-link>
       </li>
       <li class="nav-item">
-        <a
-          class="nav-link"
-          rel="tooltip"
-          title="Like us on Facebook"
-          data-placement="bottom"
-          href
-          target="_blank"
-        >
-          <i class="fab fa-facebook-square"></i>
-          <p class="d-lg-none d-xl-none">Facebook</p>
-        </a>
+        <a class="nav-link" @click="logout()">Sair</a>
       </li>
-      <li class="nav-item">
-        <a
-          class="nav-link"
-          rel="tooltip"
-          title="Follow us on Instagram"
-          data-placement="bottom"
-          href
-          target="_blank"
-        >
-          <i class="fab fa-instagram"></i>
-          <p class="d-lg-none d-xl-none">Instagram</p>
-        </a>
+    </template>
+
+    <template slot="navbar-menu" v-else>
+      <li class="nav-item" v-for="(item, index) in loggedOutLinks" :key="index">
+        <router-link :to="item.path" class="nav-link">{{item.title}}</router-link>
       </li>
     </template>
   </navbar>
